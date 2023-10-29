2c32a6e57d464a775400939fac51fb861dfa0845
Treasy79 <95193913+Treasy79@users.noreply.github.com>
2023-10-29 23:15:02 +0100

    Squashed commit of the following:
    commit 60a6155184fd5f685809b6b42893667532ea0a73
    Author: Treasy79 <95193913+Treasy79@users.noreply.github.com>
    Date:   Sun Oct 29 21:58:10 2023 +0100
    
        Update .gitignore
    
    commit 69ef4a878296dcdc1b6a1c6f67006b8bb1579e1b
    Author: Treasy79 <95193913+Treasy79@users.noreply.github.com>
    Date:   Sun Oct 29 21:57:55 2023 +0100
    
        Update .DS_Store
    
    commit 7831dacd459f72ef64ab4095d9c1b15131de2a94
    Author: Treasy79 <95193913+Treasy79@users.noreply.github.com>
    Date:   Wed Apr 19 16:48:58 2023 +0200
    
        fnmatch workaround
    
    commit 285a7d640e5029929f8c6eb22384e79f643ce565
    Author: Treasy79 <95193913+Treasy79@users.noreply.github.com>
    Date:   Fri Mar 17 23:18:47 2023 +0100
    
        Changed Sort Order
    
    commit 42505f631cfe710ce80336f922fac8daaab9fed7
    Author: Treasy79 <95193913+Treasy79@users.noreply.github.com>
    Date:   Fri Mar 17 23:12:57 2023 +0100
    
        Changed Topic Autarky - consumption
    
    commit 88ce12b5bbc7d5ce4fcfd9660ad53d345bd4c377
    Author: Treasy79 <95193913+Treasy79@users.noreply.github.com>
    Date:   Fri Mar 17 23:09:29 2023 +0100
    
        Übersetzung
    
    commit 7ccfabdbd72367f3078c58da967d1e72571cbc8d
    Author: Treasy79 <95193913+Treasy79@users.noreply.github.com>
    Date:   Fri Mar 17 22:50:42 2023 +0100
    
        Added new TAGS
    
    commit 5d68eba5c245373ede9fc8b2b8b52b353f944ecc
    Author: Treasy79 <95193913+Treasy79@users.noreply.github.com>
    Date:   Fri Mar 17 22:16:16 2023 +0100
    
        Rückbau Topic & Value für Tree Struktur
    
    commit 2ec44d932f2bd61689fbbefd9fde197770a32a1a
    Author: Treasy79 <95193913+Treasy79@users.noreply.github.com>
    Date:   Thu Mar 16 22:00:42 2023 +0100
    
        Exclude Headers from Maintain Variables
    
    commit a0058792b6ea50b7b1ba228ea1922e3e6502b7cf
    Author: Treasy79 <95193913+Treasy79@users.noreply.github.com>
    Date:   Wed Mar 15 16:52:26 2023 +0100
    
        TOPIC as const
    
    commit c25f753870ee45816f1484038a4ce58d4c82a9eb
    Author: Treasy79 <95193913+Treasy79@users.noreply.github.com>
    Date:   Mon Mar 13 22:27:33 2023 +0100
    
        Error COrrection
    
    commit f6357b6fddca6f46bfa19bab5ad3201d4493c681
    Author: Treasy79 <95193913+Treasy79@users.noreply.github.com>
    Date:   Mon Mar 13 22:20:30 2023 +0100
    
        Adjustment Form
    
    commit 1fd53134e3971c2751cd8310648bbb97110a63e0
    Author: Treasy79 <95193913+Treasy79@users.noreply.github.com>
    Date:   Mon Mar 13 22:13:28 2023 +0100
    
        Reorg List to Tree
    
    commit e483515d49934f457bc18d891bfab1994e331dc8
    Author: Treasy79 <95193913+Treasy79@users.noreply.github.com>
    Date:   Thu Mar 9 18:10:24 2023 +0100
    
        Umbau Struktur für weitere Module
    
    commit d347703aaca6543c4d736984000144319605d6d4
    Author: Treasy79 <95193913+Treasy79@users.noreply.github.com>
    Date:   Thu Mar 9 13:12:16 2023 +0100
    
        Change from TOPIC Property to Attrribute
    
    commit e13d725e5b2e4b5c8977556a9797115c4f7e0ab8
    Author: Treasy79 <95193913+Treasy79@users.noreply.github.com>
    Date:   Thu Mar 9 13:03:03 2023 +0100
    
        Topic function
    
    commit eeb167485ac483323bf823dbe9cb5a3f17bd8e80
    Author: Treasy79 <95193913+Treasy79@users.noreply.github.com>
    Date:   Wed Mar 8 12:21:33 2023 +0100
    
        Implementation of Yearly Consumptions
    
    commit 0cb054bd8e84a3c69414e477da5226192abfee31
    Author: Treasy79 <95193913+Treasy79@users.noreply.github.com>
    Date:   Tue Mar 7 12:08:29 2023 +0100
    
        Initial Commit - Struktur Umbau

---
+++ libs/Functions.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+if (!function_exists('fnmatch')) {
+    function fnmatch($pattern, $string)
+    {
+        return preg_match('#^' . strtr(preg_quote($pattern, '#'), ['\*' => '.*', '\?' => '.']) . '$#i', $string);
+    }
+}
